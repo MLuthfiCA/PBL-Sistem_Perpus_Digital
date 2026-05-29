74da52dcd31d3026aeef8b47089cf20705980e5f
Kane-98 <riskykurnia0812@gmail.com>
2026-05-29 16:29:56 +0100

    Readspace_db (VARCHAR fix).sql

---
+++ database/readspace (VARCHAR fix).sql
@@ -0,0 +1,353 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 28, 2026 at 02:55 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.3.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `readspace`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` bigint UNSIGNED NOT NULL,
+  `judul` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `penulis` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `penerbit` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tahun_terbit` year NOT NULL,
+  `isbn` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `stok` int NOT NULL DEFAULT '1',
+  `deskripsi` text COLLATE utf8mb4_unicode_ci,
+  `id_kategori` bigint UNSIGNED NOT NULL,
+  `cetakan` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `genre` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bahasa` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Indonesia',
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cover` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lokasi_rak` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tampil_katalog` tinyint(1) NOT NULL DEFAULT '1',
+  `status` enum('Tersedia','Dipinjam','Hilang','Perawatan') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Tersedia',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `judul`, `penulis`, `penerbit`, `tahun_terbit`, `isbn`, `stok`, `deskripsi`, `id_kategori`, `cetakan`, `genre`, `bahasa`, `slug`, `cover`, `lokasi_rak`, `tampil_katalog`, `status`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Laskar Pelangi', 'Andrea Hirata', 'Bentang Pustaka', '2005', '978-979-3062-79-1', 4, NULL, 1, NULL, 'Drama', 'Indonesia', 'laskar-pelangi-639', '1779981646_Laskar_pelangi_sampul.jpg', NULL, 1, 'Tersedia', '2026-04-26 23:13:34', '2026-05-28 08:32:30', NULL),
+(2, 'Bumi', 'Tere Liye', 'Gramedia Pustaka Utama', '2014', '978-602-03-3295-6', 3, NULL, 1, NULL, 'Fantasi', 'Indonesia', 'bumi-101', '1779981590_cover_buku_bumi.jpg', NULL, 1, 'Tersedia', '2026-04-26 23:13:34', '2026-05-28 08:19:50', NULL),
+(3, 'Filosofi Teras', 'Henry Manampiring', 'Kompas', '2018', '978-602-412-518-9', 9, NULL, 2, NULL, 'Self-Dev', 'Indonesia', 'filosofi-teras-270', '1779981750_filosofi_teras.webp', NULL, 1, 'Tersedia', '2026-04-26 23:13:34', '2026-05-28 08:34:21', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_peminjaman`
+--
+
+CREATE TABLE `detail_peminjaman` (
+  `id_detail` bigint UNSIGNED NOT NULL,
+  `id_peminjaman` bigint UNSIGNED NOT NULL,
+  `id_buku` bigint UNSIGNED NOT NULL,
+  `jumlah` int NOT NULL DEFAULT '1',
+  `batas_kembali_buku` date DEFAULT NULL,
+  `kondisi_kembali` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `denda_per_item` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `dikembalikan_pada` timestamp NULL DEFAULT NULL,
+  `catatan` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` bigint UNSIGNED NOT NULL,
+  `nama_kategori` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `deskripsi` text COLLATE utf8mb4_unicode_ci,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `deskripsi`, `slug`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Novel', NULL, 'novel', '2026-05-28 10:15:43', '2026-05-28 10:15:43', NULL),
+(2, 'Self Improvement', NULL, 'self-improvement', '2026-05-28 10:15:43', '2026-05-28 10:15:43', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '0001_01_01_000000_create_users_table', 1),
+(2, '0001_01_01_000001_create_kategori_table', 1),
+(3, '0001_01_01_000002_create_buku_table', 1),
+(4, '0001_01_01_000003_create_peminjaman_table', 1),
+(5, '0001_01_01_000004_create_detail_peminjaman_table', 1),
+(6, '0001_01_01_000005_create_riwayat_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_peminjaman` bigint UNSIGNED NOT NULL,
+  `id_pengguna` bigint UNSIGNED NOT NULL,
+  `tanggal_pinjam` date NOT NULL,
+  `tanggal_kembali` date DEFAULT NULL,
+  `status` enum('dipinjam','dikembalikan','terlambat') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'dipinjam',
+  `denda` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `kode_peminjaman` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `batas_kembali` date DEFAULT NULL,
+  `status_denda` enum('lunas','belum_lunas') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'lunas',
+  `catatan` text COLLATE utf8mb4_unicode_ci,
+  `id_buku` bigint UNSIGNED DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`id_peminjaman`, `id_pengguna`, `tanggal_pinjam`, `tanggal_kembali`, `status`, `denda`, `kode_peminjaman`, `batas_kembali`, `status_denda`, `catatan`, `id_buku`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 1, '2026-04-17', '2026-04-24', 'dikembalikan', 0.00, NULL, '2026-04-24', 'lunas', NULL, 3, '2026-04-26 23:13:34', '2026-04-26 23:13:34', NULL),
+(2, 1, '2026-04-22', '2026-05-01', 'dikembalikan', 0.00, NULL, '2026-04-29', 'lunas', NULL, 1, '2026-04-26 23:13:34', '2026-04-30 20:16:33', NULL),
+(3, 1, '2026-04-27', '2026-05-18', 'dikembalikan', 75000.00, NULL, '2026-05-04', 'lunas', NULL, 2, '2026-04-26 23:13:34', '2026-05-18 06:21:27', NULL),
+(4, 1, '2026-05-28', NULL, 'dipinjam', 0.00, NULL, '2026-06-04', 'lunas', NULL, 1, '2026-05-28 08:32:30', '2026-05-28 08:32:30', NULL),
+(5, 3, '2026-06-03', NULL, 'dipinjam', 0.00, NULL, '2026-06-10', 'lunas', NULL, 3, '2026-05-28 08:34:21', '2026-05-28 08:34:21', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `riwayat`
+--
+
+CREATE TABLE `riwayat` (
+  `id_riwayat` bigint UNSIGNED NOT NULL,
+  `id_pengguna` bigint UNSIGNED NOT NULL,
+  `id_peminjaman` bigint UNSIGNED DEFAULT NULL,
+  `tanggal` date NOT NULL,
+  `aktivitas` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `deskripsi` text COLLATE utf8mb4_unicode_ci,
+  `ip_address` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_agent` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_pengguna` bigint UNSIGNED NOT NULL,
+  `nama` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` enum('admin','mahasiswa') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'mahasiswa',
+  `identity_number` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `username` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` enum('active','inactive','suspended') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'active',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_pengguna`, `nama`, `email`, `password`, `role`, `identity_number`, `username`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Rayyan', 'rayyan@student.polibatam.ac.id', '$2y$12$w8zfDFuV5gBvziMW8X0sX.s9XmmHdp94/HtTDk0/ojO.NdHzAGcFq', 'mahasiswa', 'ID-00001', 'rayyan123', 'active', '2026-04-26 23:03:58', '2026-04-26 23:03:58'),
+(2, 'Admin Readspace', 'admin@readspace.com', '$2y$12$43aI91WpKks8CKNkq0.sA.y8Kw.ivYppLcAEqHM7BsTpqr8v6fVI2', 'admin', 'ID-00002', 'admin', 'active', '2026-05-10 02:30:44', '2026-05-10 02:30:44'),
+(3, 'Student Readspace', 'student@readspace.com', '$2y$12$VqSB6yLGk6y/ctuq.vWPMO2Bil4AgJnT1US7N3ZspVm.BdHsFOY8O', 'mahasiswa', 'ID-00003', 'student', 'active', '2026-05-10 02:30:45', '2026-05-10 02:30:45');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`),
+  ADD UNIQUE KEY `buku_isbn_unique` (`isbn`),
+  ADD UNIQUE KEY `buku_slug_unique` (`slug`),
+  ADD KEY `buku_id_kategori_foreign` (`id_kategori`);
+
+--
+-- Indexes for table `detail_peminjaman`
+--
+ALTER TABLE `detail_peminjaman`
+  ADD PRIMARY KEY (`id_detail`),
+  ADD KEY `detail_peminjaman_id_peminjaman_foreign` (`id_peminjaman`),
+  ADD KEY `detail_peminjaman_id_buku_foreign` (`id_buku`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`),
+  ADD UNIQUE KEY `kategori_nama_kategori_unique` (`nama_kategori`),
+  ADD UNIQUE KEY `kategori_slug_unique` (`slug`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`),
+  ADD UNIQUE KEY `peminjaman_kode_peminjaman_unique` (`kode_peminjaman`),
+  ADD KEY `peminjaman_id_pengguna_foreign` (`id_pengguna`),
+  ADD KEY `peminjaman_id_buku_foreign` (`id_buku`);
+
+--
+-- Indexes for table `riwayat`
+--
+ALTER TABLE `riwayat`
+  ADD PRIMARY KEY (`id_riwayat`),
+  ADD KEY `riwayat_id_pengguna_foreign` (`id_pengguna`),
+  ADD KEY `riwayat_id_peminjaman_foreign` (`id_peminjaman`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_pengguna`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD UNIQUE KEY `users_identity_number_unique` (`identity_number`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `detail_peminjaman`
+--
+ALTER TABLE `detail_peminjaman`
+  MODIFY `id_detail` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_peminjaman` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `riwayat`
+--
+ALTER TABLE `riwayat`
+  MODIFY `id_riwayat` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_pengguna` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `buku`
+--
+ALTER TABLE `buku`
+  ADD CONSTRAINT `buku_id_kategori_foreign` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `detail_peminjaman`
+--
+ALTER TABLE `detail_peminjaman`
+  ADD CONSTRAINT `detail_peminjaman_id_buku_foreign` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE CASCADE,
+  ADD CONSTRAINT `detail_peminjaman_id_peminjaman_foreign` FOREIGN KEY (`id_peminjaman`) REFERENCES `peminjaman` (`id_peminjaman`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_id_buku_foreign` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE SET NULL,
+  ADD CONSTRAINT `peminjaman_id_pengguna_foreign` FOREIGN KEY (`id_pengguna`) REFERENCES `users` (`id_pengguna`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `riwayat`
+--
+ALTER TABLE `riwayat`
+  ADD CONSTRAINT `riwayat_id_peminjaman_foreign` FOREIGN KEY (`id_peminjaman`) REFERENCES `peminjaman` (`id_peminjaman`) ON DELETE SET NULL,
+  ADD CONSTRAINT `riwayat_id_pengguna_foreign` FOREIGN KEY (`id_pengguna`) REFERENCES `users` (`id_pengguna`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
